688e16ef333e75799dadb2de1986340d3900a358
unknown <miguel.monroy-c@labinfo.is.escuelaing.edu.co>
2025-09-20 09:52:49 -0500

    correccion importaciones

--- Arquitectura_Web/src/main/java/com/mycompany/controller/ServiceController.java
+++ Arquitectura_Web/src/main/java/com/mycompany/controller/ServiceController.java
@@ -2,15 +2,17 @@ package com.mycompany.controller;
 
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClient;
+import org.springframework.beans.factory.aot.AotServices.Loader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-//APIKEY = XUD2YQOG2P0LWY3E
 
 @RestController
-@RequestMapping(value = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey="+APIKEY)
+@RequestMapping(value = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=")
 
 public class ServiceController {
 
@@ -21,7 +23,7 @@ public class ServiceController {
         try {
             return new ResponseEntity<>(data,HttpStatus.ACCEPTED);
         } catch (Exception ex) {
-            Logger.getLogger(XXController.class.getName()).log(Level.SEVERE, null, ex);
+            Loader.getLogger(XXController.class.getName()).log(Level.SEVERE, null, ex);
             return new ResponseEntity<>("Error bla bla bla",HttpStatus.NOT_FOUND);
         }        
     }
